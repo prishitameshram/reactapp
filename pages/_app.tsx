2fdf6dd75e53e0229ea46d0ea972de2beb834d80
prishitameshram <54324852+prishitameshram@users.noreply.github.com>
2023-12-01 16:32:12 -0600

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,23 +1,10 @@
-// Import the necessary modules and styles
-// import '../styles/tailwind.css';
-// import '../node_modules/tailwindcss/base.css';
-// import 'tailwindcss/components.css';
-// import 'tailwindcss/utilities.css';
-// import '../styles';
+// _app.tsx
+import 'tailwindcss/tailwind.css';
 
+interface PageProps {}
 
-import 'tailwindcss/tailwind.css'
-
-// Define the shape of the pageProps
-interface PageProps {
-  // define the shape of pageProps here
-}
-
-// MyApp component that receives the Component and pageProps as props
 function MyApp({ Component, pageProps }: { Component: any, pageProps: PageProps }) {
-  // Render the component with its props
   return <Component {...pageProps} />;
 }
 
-// Export the MyApp component as the default export
 export default MyApp;
