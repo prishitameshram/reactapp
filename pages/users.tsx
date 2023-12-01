01c887f9455ce952d7db7611134f50ba6fbb623d
prishitameshram <54324852+prishitameshram@users.noreply.github.com>
2023-12-01 16:33:08 -0600

    Update users.tsx

--- pages/users.tsx
+++ pages/users.tsx
@@ -1,5 +1,4 @@
 // pages/users.tsx
-// import '../styles/tailwind.css';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 import { User } from '../types/user';
@@ -23,13 +22,13 @@ const UsersPage: React.FC = () => {
 
   return (
     <div className="container mx-auto my-8 flex flex-col items-center">
-  <h1 className="text-2xl font-bold mb-4">User List</h1>
-  <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-    {users.map((user) => (
-      <UserCard key={user.id} user={user} />
-    ))}
-  </div>
-</div>
+      <h1 className="text-2xl font-bold mb-4">User List</h1>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+        {users.map((user) => (
+          <UserCard key={user.id} user={user} />
+        ))}
+      </div>
+    </div>
   );
 };
 
