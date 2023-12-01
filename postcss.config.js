1355a4c1461af0309a6b3626f181bc98414b5f4e
prishitameshram <54324852+prishitameshram@users.noreply.github.com>
2023-12-01 17:56:22 -0600

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,10 +1,6 @@
-module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-    // Add cssnano for minification in production
-    ...(process.env.NODE_ENV === 'production'
-      ? { cssnano: { preset: 'default' } }
-      : {}),
-  },
-};
+module.exports = { 
+	plugins: { 
+		tailwindcss: {}, 
+		autoprefixer: {}, 
+	}, 
+}
