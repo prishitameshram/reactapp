5c4561c51d4758d5e0fb262ec45ebec181384d03
prishitameshram <54324852+prishitameshram@users.noreply.github.com>
2023-12-01 16:32:32 -0600

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,8 +5,7 @@ const HomePage: React.FC = () => {
   return (
     <div className="container mx-auto my-8 text-center">
       <h1 className="text-4xl font-bold mb-4">Welcome to User List Viewer</h1>
-      
-      <Link className="text-blue-500 underline" href="/users">
+      <Link href="/users" className="text-blue-500 underline">
         View User List
       </Link>
     </div>
